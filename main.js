1ac1a12fa284ee1bc4991c3e4b7d4c1c90a97d79
yuetsh <517252939@qq.com>
2023-12-06 16:24:33 +0800

    update.

--- main.js
+++ main.js
@@ -2,35 +2,35 @@ import "./style.css"
 
 const sites = [
   {
-    url: "https://oj.hyyz.izhai.net",
+    url: "https://oj.xuyue.cc",
     title: "在线判题网站",
     description: "判题狗",
   },
   {
-    url: "https://code.hyyz.izhai.net",
+    url: "https://code.xuyue.cc",
     title: "代码运行网站",
     description: "自测猫",
   },
-  {
-    url: "https://book.hyyz.izhai.net",
-    title: "编程知识网站",
-    description: "编程书",
-  },
-  {
-    url: "https://play.hyyz.izhai.net",
-    title: "格式练习网站",
-    description: "练习册",
-  },
-  {
-    url: "https://ppt.hyyz.izhai.net/py",
-    title: "Python PPT",
-    description: " Python 课件",
-  },
-  {
-    url: "https://huabu.hyyz.izhai.net",
-    title: "画布",
-    description: "在线板书",
-  },
+  // {
+  //   url: "https://book.xu'yue",
+  //   title: "编程知识网站",
+  //   description: "编程书",
+  // },
+  // {
+  //   url: "https://play.hyyz.izhai.net",
+  //   title: "格式练习网站",
+  //   description: "练习册",
+  // },
+  // {
+  //   url: "https://ppt.hyyz.izhai.net/py",
+  //   title: "Python PPT",
+  //   description: " Python 课件",
+  // },
+  // {
+  //   url: "https://huabu.hyyz.izhai.net",
+  //   title: "画布",
+  //   description: "在线板书",
+  // },
 ]
 
 const item = (site) => `
@@ -43,7 +43,7 @@ const item = (site) => `
 document.querySelector("#app").innerHTML = `
 <div class="container">
   <main class="main">
-    <h1 class="title">你是不是要找以下网站？</h1>
+    <h1 class="title">徐越的在线教学平台</h1>
     <div class="grid" id="sites"></div>
   </main>
 </div>
